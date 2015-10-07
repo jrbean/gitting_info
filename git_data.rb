e24cff2a825167dc58d7819b808c18998c0a695c
Justin Bean <beanjustinr@gmail.com>
2015-10-06 21:43:43 -0400

    removed comments

--- git_data.rb
+++ git_data.rb
@@ -2,16 +2,7 @@ require "pry"
 require "httparty"
 
 
-# puts r.message
-# puts r.code
 
-# class User
-#
-#   def initialize users
-#     @users = users
-#   end
-#
-#   def user_list
 key = File.read("./git_secret")
 response = HTTParty.get "https://api.github.com/orgs/Rails/members", headers: {
     "Authorization" => "token #{key}",
@@ -27,10 +18,3 @@ response.each do |us| us["login"]
       puts "#{rget["names"]}"
     end
   end
-
-
-
-
-
-binding.pry
-# end
